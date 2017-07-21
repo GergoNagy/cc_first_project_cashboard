991a37362a813d03c08a1e3052f5ed6299672fed
Gergo Nagy <guzmop@gmail.com>
2017-07-21 12:07:57 +0100

    db created

---
+++ db/cashboards.sql
@@ -0,0 +1,26 @@
+DROP TABLE transactions;
+DROP TABLE tags;
+DROP TABLE merchants;
+
+
+CREATE TABLE tags
+(
+  id SERIAL8 PRIMARY KEY,
+  type VARCHAR(255)
+);
+
+CREATE TABLE merchants
+(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  icon VARCHAR(255)
+);
+
+CREATE TABLE transactions
+(
+  id SERIAL8 PRIMARY KEY,
+  value INT4,
+  trans_date date,
+  tag_id INT8 REFERENCES tags(id),
+  merchant_id INT8 REFERENCES merchants(id)
+);
