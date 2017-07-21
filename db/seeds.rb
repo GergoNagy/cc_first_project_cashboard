f497ce0f905a66ec7fb8b42113a653bd02521953
Gergo Nagy <guzmop@gmail.com>
2017-07-21 15:36:58 +0100

    controllers files created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Tag.delete_all()
 Merchant.delete_all()
 
 tag1 = Tag.new({'type' => 'food'})
-tag2 = Tag.new({'type' => 'drink'})
+tag2 = Tag.new({'type' => 'cloats'})
 tag3 = Tag.new({'type' => 'IT stuff'})
 tag4 = Tag.new({'type' => 'rent'})
 
@@ -25,5 +25,12 @@ merchant2.save
 merchant3.save
 
 transaction1 = Transaction.new({'value' => 12.55, 'tag_id' => tag1.id, 'merchant_id' => merchant1.id})
+transaction2 = Transaction.new({'value' => 1200, 'tag_id' => tag3.id, 'merchant_id' => merchant2.id})
+transaction3 = Transaction.new({'value' => 200, 'tag_id' => tag2.id, 'merchant_id' => merchant3.id})
 
 transaction1.save
+transaction2.save
+transaction3.save
+
+
+
